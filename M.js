1cc773d40b41354980e20e2aec7609b21615239b
OBED-4k <mikelorsd299@gmail.com>
2026-05-06 09:46:02 -0400

    first commit

---
+++ M.js
@@ -0,0 +1,124 @@
+const form = document.querySelector("form");
+
+const nameError = document.getElementById("use");
+const emailError = document.getElementById("ema");
+const numError = document.getElementById("num");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+const name = document.getElementById("Name").value.trim();
+  const email = document.getElementById("Email").value.trim();
+  const number = document.getElementById("Number").value.trim();
+
+  console.log(name, email, number);
+
+  nameError.textContent = "";
+  emailError.textContent = "";
+  numError.textContent = "";
+
+   // NAME
+  if (name.length < 5) {
+    nameError.textContent = "Enter full name";
+    nameError.style.color = "red"
+    return;
+  }
+  // EMAIL
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    emailError.textContent = "Enter a valid email";
+    emailError.style.color = "red"
+    return;
+  }
+
+  // PHONE
+    const allowedCodes = ["+234", "+1", "+44", "+91", "+81", "+61"];
+  const validCode = allowedCodes.some(code => number.startsWith(code));
+
+  if (!validCode) {
+    numError.textContent = "Use valid country code (+234 etc)";
+    numError.style.color = "red"
+    return;
+  }
+
+  if (number.length < 10) {
+    numError.textContent = "Invalid number length";
+    numError.style.color = "red"
+    return;
+  }
+
+  // SUCCESS
+  console.log("Submitted:", { name, email, number });
+
+  window.location.href = "page 2.html";
+});
+  
+
+    
+
+
+
+
+
+
+
+// const form = document.querySelector("form");
+// // const feedBack = document.getElementById("diff");
+
+// const nameError = document.getElementById("use");
+// const emailError = document.getElementById("ema");
+// const numError = document.getElementById("num");
+
+// const name = document.getElementById("Name").value.trim();
+// const email = document.getElementById("Email").value.trim();
+// const number = document.getElementById("Number").value.trim();
+// const Button = document.querySelector("button")
+
+// console.log(name, email, number);
+
+
+// nameError.textContent = "";
+// emailError.textContent = "";
+// numError.textContent = "";
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// // NAME VALIDATION
+//   if (name.length < 5) {
+//     nameError.textContent = "Enter your full name";
+//     nameError.style.color = "red";
+//     return;
+//   } 
+// //EMAIL VALIDATION
+// //   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// //   if (!emailPattern.test(email)) {
+// //     feedBack.textContent = "Enter a valid email (must include @ and .com)";
+// //     feedBack.style.color = "red";
+// //     return;
+// const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (!emailPattern.test(email)) {
+//     emailError.textContent = "Enter a valid email (must include @ and .com)";
+//     emailError.style.color = "red";
+//     return;
+//   } 
+
+//   //  PHONE VALIDATION (with country codes)
+//   const allowedCodes = ["+234", "+1", "+44", "+91", "+81", "+61"];
+  
+//   const validCode = allowedCodes.some(code => number.startsWith(code));
+//   if(!validCode){
+//     numError.textContent = "Use a Valid country code (e.g +234)";
+//   }
+
+//   if (number.length < 10) {
+//     numError.textContent = "Invalid number length";
+//     numError.style.color = "red";
+//     return;
+//   } 
+//   feedBack.textContent = "Login successful";
+//   feedBack.style.color = "green";
+//   console.log("Submitted:", { name, email, number});
+
+//   window.location.href = "./page 2.html";
+// });
+  
